aa7ef3373ec3b34990a911e0fa548042ee4fee77
Mukesh Yadav <72705190+MK-Brightpath-Academy@users.noreply.github.com>
2025-11-02 00:46:19 +0530

    Time: 1 ms (25.00%) | Memory: 46.5 MB (7.64%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+          List<Integer> result = new ArrayList<>();
+        traverse(root, result);
+        return result;
+    }
+
+    private void traverse(Node node, List<Integer> result) {
+        if (node == null) return;
+
+        // 1️⃣ Visit all children first
+        for (Node child : node.children) {
+            traverse(child, result);
+        }
+
+        // 2️⃣ Then visit the current node
+        result.add(node.val);
+    }
+}

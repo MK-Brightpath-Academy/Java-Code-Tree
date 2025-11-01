aff154794f995d017d0a0a2ad24f4aa846f05cad
Mukesh Yadav <72705190+MK-Brightpath-Academy@users.noreply.github.com>
2025-11-02 00:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (5.08%) - LeetSync

---
+++ 933-increasing-order-search-tree/increasing-order-search-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+          Stack<TreeNode> stack = new Stack<>();
+        TreeNode dummy = new TreeNode(-1);
+        TreeNode current = dummy;
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+
+            // Visit node
+            root.left = null;
+            current.right = root;
+            current = root;
+
+            root = root.right;
+        }
+
+        return dummy.right;
+    }
+}
